c1a73701f1026ecfa25cd9cce5cca9ff0713aaa9
Suraj <surajmail.799@gmail.com>
2026-07-05 10:51:44 +0530

    complete authentication flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+'use client'
+import { UserMenuWithSession } from "@/features/auth/components/user-menu";
+import { authClient } from "@/lib/auth-client";
 // import { ModeToggle } from "@/components/ui/mode-toggle";
 import Image from "next/image";
 
 export default function Home() {
+  const {data} = authClient.useSession()
+  console.log("data", data?.user)
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <UserMenuWithSession variant="compact" />
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
