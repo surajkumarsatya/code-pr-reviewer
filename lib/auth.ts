7994c806863cefe68bdf4ee1f73d5a11965700ad
Suraj <surajmail.799@gmail.com>
2026-07-06 12:56:42 +0530

    add github connection

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,15 +4,20 @@ import { prisma } from "./db";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-     database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
-    socialProviders: { 
-        github: { 
-        clientId: process.env.GITHUB_CLIENT_ID as string, 
-        clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
-        }, 
-  }, 
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
 
-   plugins: [nextCookies()]
+      mapProfileToUser:async(profile)=>({
+        email:profile.email ?? `${profile.id}@users.noreply.github.com`,
+        name:profile.name ?? profile.login,
+      })
+    },
+  },
+
+  plugins: [nextCookies()],
 });
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['seventh-cross-chummy.ngrok-free.dev'],
 };
 
 export default nextConfig;
